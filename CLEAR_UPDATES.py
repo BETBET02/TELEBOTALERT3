0a0d40cc1b1e216789e8ccd025ea329c06b03eb6
BETBET02 <rikualakoski00@gmail.com>
2025-07-22 03:00:14 +0300

    CLEAR_UPDATES.py

---
+++ CLEAR_UPDATES.py
@@ -0,0 +1,13 @@
+import os
+import asyncio
+from aiogram import Bot
+
+async def clear_updates():
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    bot = Bot(token=TELEGRAM_TOKEN)
+    await bot.delete_webhook(drop_pending_updates=True)
+    print("Webhook deleted and pending updates cleared!")
+    await bot.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(clear_updates())
