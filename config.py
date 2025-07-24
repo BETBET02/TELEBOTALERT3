bbb5d7d8425035b5be4b928f6936accb87d2e85d
BETBET02 <rikualakoski00@gmail.com>
2025-07-25 01:56:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "").strip()
 SPORTSRADAR_API_KEY = os.getenv("SPORTSRADAR_API_KEY", "").strip()
 THRESHOLD_PERCENT = float(os.getenv("THRESHOLD_PERCENT", "20"))
