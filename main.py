42444f1885a0c55083937617d40f1968de1e4773
BETBET02 <rikualakoski00@gmail.com>
2025-07-26 00:12:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
+from commands.ottelut import ottelut
 from commands.kerroinmuutokset import kerroinmuutokset
+import logging
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def main():
-    app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+logging.basicConfig(level=logging.INFO)
 
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("ottelut", ottelut))
     app.add_handler(CommandHandler("kerroinmuutokset", kerroinmuutokset))
 
-from commands.ottelut import ottelut
-app.add_handler(CommandHandler("ottelut", ottelut))
-
-app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
