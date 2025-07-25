f684c83bf59b75aec736acf855fc293227eba620
BETBET02 <rikualakoski00@gmail.com>
2025-07-25 03:10:56 +0300

    Update leagues.py

--- leagues.py
+++ leagues.py
@@ -1,26 +1,8 @@
 LEAGUES = {
-    "brasilia_serie_a": {
-        "competition_id": 325,   # sr:competition:325
-        "season_id": 128461      # sr:season:128461
-    },
-    "epl": {
-        "competition_id": 17,
-        "season_id": 130281
-    },
-    "laliga": {
-        "competition_id": 8,
-        "season_id": 130805
-    },
-    "serie_a": {
-        "competition_id": 23,
-        "season_id": 130971
-    },
-    "bundesliga": {
-        "competition_id": 35,
-        "season_id": 130571
-    },
-    "ligue_1": {
-        "competition_id": 34,
-        "season_id": 131609
-    }
+    "brasilia": "sr:season:128461",
+    "valioliiga": "sr:season:130281",
+    "laliga": "sr:season:130805",
+    "seriea": "sr:season:130971",
+    "bundesliga": "sr:season:130571",
+    "ligue1": "sr:season:131609"
 }
